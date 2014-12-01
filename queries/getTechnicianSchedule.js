09deb8b626f109e58be81928d82f4bd5f01376e0
Michael Learo <mlearo@gmail.com>
2014-11-30 17:16:12 -0800

    worte new query which allowed to reduce a method call from process while still completing the same task

--- queries/getTechnicianSchedule.js
+++ queries/getTechnicianSchedule.js
@@ -24,12 +24,14 @@ module.exports.createTechnicianSchedule = function createTechnicianSchedule(req,
 			console.log(err + 'error finding technician information');
 			res.redirect('/');
 		}
-		res.locals.technicianInformation = technicians; // put technician information retrived from database in local variable
-		getCustomerInfo(req, res);		
+		res.locals.technicianInfo = technicians; // put technician information retrived from database in local variable
+		//getCustomerInfo(req, res);		
+		getSchedule(req, res);
 	})
 };
 
 
+/*  Refactored code, these two functions are no longer needed
 
 var getCustomerInfo = function getCustomerInfo(req, res, next){
 
@@ -64,6 +66,22 @@ var getCustomerScheduleInformation = function getCustomerScheduleInformation(req
 	});
 }
 
+
+*/
+var getSchedule = function getSchedule(req, res) {
+
+	var scheduleQuery = "select distinct customer.customer_firstname, customer.customer_lastname, customer.customer_address, customer.customer_zipcode, customer.customer_city, customer.customer_id, scheduledServices.service_date, technician.technician_id, technician.technician_name from scheduledServices join (customer, technician) on (technician.technician_id = customer.technician_id and customer.customer_id = scheduledServices.customer_id) where scheduledServices.service_date >= '10/01/2014' and scheduledServices.service_date <= '10/31/2014';"
+
+	connection.query(scheduleQuery, function(err, schedule){
+		if(err) {
+			console.log(err + 'error finding technician information');
+			res.redirect('/');
+		}
+		res.locals.schedule = schedule;
+		console.log(schedule);
+		res.render('tech', {schedule: res.locals.schedule, technicians: res.locals.technicianInfo, workweek: workWeek});
+	})
+}
 
 
 
